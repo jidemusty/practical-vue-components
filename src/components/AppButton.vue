0b51c2247c37b629e53682ce307da2badb5be292
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-10-23 18:43:36 -0500

    basic button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <router-link 
+        class="bg-blue px-5 py-2 border-none no-underline rounded text-white"
+        :to=to
+    >
+        Sign In
+    </router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        to: {
+            required: false,
+            type: Object,
+            default () {
+                return {
+                    name: 'signin'
+                }
+            }
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AppButton />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppButton from '@/components/AppButton'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    AppButton
   }
 }
 </script>
