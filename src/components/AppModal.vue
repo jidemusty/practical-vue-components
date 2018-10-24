07cbd1c62e7a6df58329d808e44c747d3345633c
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-10-24 13:38:22 -0500

    modal: simple closing

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="app-modal" v-if="visible">
+    <div v-if="visible">
+        <div class="app-modal" @click.prevent="visible = false"></div>
         <div class="app-modal-inner">
+            <a href="#" @click.prevent="visible = false">Close</a>
             <slot />
         </div>
     </div>
@@ -8,48 +10,49 @@
 
 <script>
 export default {
-    data() {
-        return {
-            visible: false
-        }
-    },
-    props: {
-        name: {
-            required: true,
-            type: String
-        }
-    },
-    mounted () {
-        this.$modal.$event.$on('show', (modal) => {
-            if (this.name === modal) {
-                this.visible = true
-            }
-        })
+  data() {
+    return {
+      visible: false
+    };
+  },
+  props: {
+    name: {
+      required: true,
+      type: String
     }
-}
+  },
+  mounted() {
+    this.$modal.$event.$on("show", modal => {
+      if (this.name === modal) {
+        this.visible = true;
+      }
+    });
+  }
+};
 </script>
 
 
 <style>
-    .app-modal {
-        background-color: #141420;
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        z-index: 9999;
-        top: 0;
-        left: 0; 
-    }
+.app-modal {
+  background-color: #141420;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+}
 
-    .app-modal-inner {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: #FFF;
-        padding: 30px;
-        width: 90%;
-        max-width: 500px;
-        border-radius: 3px; 
-    }
+.app-modal-inner {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 30px;
+  width: 90%;
+  max-width: 500px;
+  border-radius: 3px;
+  z-index: 9999;
+}
 </style>
