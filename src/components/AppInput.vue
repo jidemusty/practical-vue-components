a0f3bf1fb107d4f1b1ef79acd648b4f292f38f2c
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-10-23 22:51:36 -0500

    type toggling

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,14 +1,25 @@
 <template>
-    <input
-        :type="type"
-        :name="name"
-        :id="name"
-        class=""
-    />
+    <div>
+        <input
+            :type="inputType"
+            :name="name"
+            :id="name"
+            class="block mb-2 p-3 shadow-outline w-full"
+        />
+
+        <a href="#" v-if="type == 'password'" @click.prevent="passwordRevealed = !passwordRevealed">
+            {{ passwordRevealed ? 'Hide' : 'Reveal' }} Password
+        </a>
+    </div>
 </template>
 
 <script>
 export default {
+    data () {
+        return {
+            passwordRevealed: false
+        }
+    },
     props: {
         type: {
             required: false,
@@ -18,6 +29,16 @@ export default {
         name: {
             required: true,
             type: String,
+        }
+    },
+    computed: {
+        inputType () {
+            return this.passwordRevealed ? 'text' : this.type
+        }
+    },
+    methods: {
+        toggleVisibility () {
+            this.inputType = this.inputType === 'password' ? 'text' : 'password'
         }
     }
 }
